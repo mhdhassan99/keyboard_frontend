39f21e4157a8e57acfa0dcc1669ce52d64d6a1e6
mhdhassan99 <mhassan.work99@gmail.com>
2020-08-12 16:47:25 -0400

    initial rendering

--- src/components/ItemCard.js
+++ src/components/ItemCard.js
@@ -5,7 +5,13 @@ class ItemCard extends React.Component {
 
     render() {
         return (
+        <div>
             <h1>{this.props.item.name}</h1>
+            <h3>Price: ${this.props.item.price}</h3>
+            <img alt="" src={this.props.item.image}/>
+            <p>Category: {this.props.item.category}</p>
+            
+        </div>
         )
     }
 }
