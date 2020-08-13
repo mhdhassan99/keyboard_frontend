c3166da8662f28a3d12d14e2ed71ea4756b76c99
Bearinawolfpack <bearinawolfpack@gmail.com>
2020-08-13 19:22:08 -0400

    Pull to push

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,49 +1,51 @@
-import React from 'react'
-import {NavLink} from 'react-router-dom'
-import {Button} from 'react-bootstrap'
-import {Navbar} from 'react-bootstrap'
-import {Nav} from 'react-bootstrap'
-import { NavDropdown} from 'react-bootstrap'
-import {Form} from 'react-bootstrap'
-import { FormControl} from 'react-bootstrap'
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { Button } from "react-bootstrap";
+import { Navbar } from "react-bootstrap";
+import { Nav } from "react-bootstrap";
+import { NavDropdown } from "react-bootstrap";
+import { Form } from "react-bootstrap";
+import { FormControl } from "react-bootstrap";
 
-function NavBar () {
-    return (
-        <ul>
-        <div className="NavBar">
-            {/* <NavLink to="/items"> <li>Home </li></NavLink>  */}
-            
-                <Navbar bg="light" expand="lg">
-                    <Navbar.Brand href="#home">Home</Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="mr-auto">
-                            <Nav.Link href="/login">Login</Nav.Link>
-                            <Nav.Link href="/items">Home</Nav.Link>
-                            <Nav.Link href="/items/keyboards">Keyboard</Nav.Link>
-                            <Nav.Link href="/accessories">Accessories</Nav.Link>
+function NavBar() {
+	return (
+		<ul>
+			<div className="NavBar">
+				{/* <NavLink to="/items"> <li>Home </li></NavLink>  */}
 
-                            {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+				<Navbar bg="light" expand="lg">
+					<Navbar.Brand href="#home">Home</Navbar.Brand>
+					<Navbar.Toggle aria-controls="basic-navbar-nav" />
+					<Navbar.Collapse id="basic-navbar-nav">
+						<Nav className="mr-auto">
+							<Nav.Link href="/login">Login</Nav.Link>
+							<Nav.Link href="/items">Home</Nav.Link>
+							<Nav.Link href="/items/keyboards">
+								Keyboard
+							</Nav.Link>
+							<Nav.Link href="/accessories">Accessories</Nav.Link>
+
+							{/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                                 <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                                 <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
                                 <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
                                 <NavDropdown.Divider />
                                 <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
                             </NavDropdown> */}
-                        </Nav>
-                        <Form inline>
-                            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-                            <Button variant="outline-success">Search</Button>
-                        </Form>
-                    </Navbar.Collapse>
-                </Navbar>
-                   
-               
-               
-        </div>
-        </ul>
-
-    )
+						</Nav>
+						<Form inline>
+							<FormControl
+								type="text"
+								placeholder="Search"
+								className="mr-sm-2"
+							/>
+							<Button variant="outline-success">Search</Button>
+						</Form>
+					</Navbar.Collapse>
+				</Navbar>
+			</div>
+		</ul>
+	);
 }
 
-export default NavBar
+export default NavBar;
