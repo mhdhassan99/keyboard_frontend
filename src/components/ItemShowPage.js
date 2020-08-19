62b4aa3c307def87322933c31425a780ec5ea6d0
mhdhassan99 <mhassan.work99@gmail.com>
2020-08-19 11:14:53 -0400

    working on video

--- src/components/ItemShowPage.js
+++ src/components/ItemShowPage.js
@@ -10,19 +10,34 @@ class ItemShowPage extends React.Component {
     }
     render() {
         return (
+          <div>
             <div className="Grid">
-                <div className="image-wrapper">
-                    <img className="item-show-page-picture" alt="" src={this.item.image} />
-                </div>
-                <div className="item-show-page">
-                    <NavLink to={`/items/${this.item.id}`}> <h1>{this.item.name}</h1> </NavLink>
-                    <h3>Price: ${this.item.price}</h3>
-                    
-                    <p>Category: {this.item.category}</p>  
-                    <button className="add-to-cart"> Add to cart </button>
-                </div>
+              <div className="image-wrapper">
+                <img
+                  className="item-show-page-picture"
+                  alt=""
+                  src={this.item.image}
+                />
+              </div>
+              <div className="item-show-page">
+                <NavLink to={`/items/${this.item.id}`}>
+                  {" "}
+                  <h1>{this.item.name}</h1>{" "}
+                </NavLink>
+                <h3>Price: ${this.item.price}</h3>
+
+                <p>Category: {this.item.category}</p>
+                <button className="add-to-cart"> Add to cart </button>
+              </div>
             </div>
-        )
+
+            <div className="video" >
+              <video className="video-box" controls width="250" crossOrigin="use-credentials">
+                <source src={this.item.video} type="video/mp4" />
+              </video>
+            </div>
+          </div>
+        );
     }
 }
 
